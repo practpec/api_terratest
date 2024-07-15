d8540ff20290a2135567bf367279e190b29896c1
Julio Perez <140465005+practpec@users.noreply.github.com>
2024-07-15 00:42:03 -0600

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/login', userController.login);
-router.post('/', userController.createUser);
+router.post('/register', userController.register);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
